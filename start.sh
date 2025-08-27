014e9382ec11b77ae514fd7c953161e44a5c60c7
Erik Vieira <erik.vieiradev@hotmail.com>
2025-08-27 14:25:13 -0300

    chore: Remove temporary start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-echo "--- Iniciando script start.sh ---"
-
-echo "Instalando navegadores do Playwright..."
-playwright install chromium
-
-echo "Iniciando o servidor Uvicorn..."
-uvicorn main:app --host 0.0.0.0 --port $PORT
